beccad55fe57f1fcf6e069e736ad11fcbd1e2b55
Javid <javiddeveloper@gmail.com>
2026-07-22 01:58:25 +0330

    feat: deploy frontend client and update nginx config

--- front_client/next.config.ts
+++ front_client/next.config.ts
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '93.127.223.93',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'web',
         port: '8000',
         pathname: '/media/**',
       },
